910c845fb6f98cc93e27dfc7063e99dfc528f3d7
bailaoye <jeffbai996@gmail.com>
2019-12-06 09:40:58 +0000

    added comments to hello.js

--- hello.js
+++ hello.js
@@ -5,3 +5,9 @@ const sayHello  = function (name) {
 sayHello("Caliban");
 sayHello("Miranda");
 sayHello("Ferdinand");
+
+// For the rest of this exercise, we will
+// be using functions that return a value. 
+// This means that we will have to console.log
+// our values outside of the function.
+
